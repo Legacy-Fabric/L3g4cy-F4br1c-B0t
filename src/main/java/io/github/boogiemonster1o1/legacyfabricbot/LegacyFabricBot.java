73ccaa50e9f7d523683189024333f0636efe2445
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-06 10:35:10 +0530

    Config manager
     Changes to be committed:
    	modified:   src/main/java/io/github/boogiemonster1o1/legacyfabricbot/LegacyFabricBot.java
    	new file:   src/main/java/io/github/boogiemonster1o1/legacyfabricbot/command/CommandManager.java

--- src/main/java/io/github/boogiemonster1o1/legacyfabricbot/LegacyFabricBot.java
+++ src/main/java/io/github/boogiemonster1o1/legacyfabricbot/LegacyFabricBot.java
@@ -5,16 +5,19 @@ import java.util.Objects;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import io.github.boogiemonster1o1.legacyfabricbot.command.CommandManager;
 import io.github.boogiemonster1o1.legacyfabricbot.config.ConfigManager;
 import io.github.boogiemonster1o1.legacyfabricbot.object.Config;
 
 public class LegacyFabricBot {
     private static LegacyFabricBot instance;
     private final ConfigManager<Config> configManager;
+    private final CommandManager commandManager;
     private GatewayDiscordClient client;
 
     private LegacyFabricBot(Path configPath) {
         this.configManager = ConfigManager.createJankson(configPath, Config.CODEC, Config.DEFAULT);
+        this.commandManager = new CommandManager();
     }
 
     public static void main(String[] args) {
@@ -24,17 +27,22 @@ public class LegacyFabricBot {
     }
 
     private void finish() {
-        this.client.onDisconnect().block();
+        this.getConfigManager().serializeQuietly();
     }
 
     private void init() {
         DiscordClient discordClient = DiscordClient.builder(this.getConfig().getTokens().getBotToken())
                 .build();
         this.client = Objects.requireNonNull(discordClient.login().block());
+        this.client.onDisconnect().block();
     }
 
     public ConfigManager<Config> getConfigManager() {
         return this.configManager;
+    }
+
+    public CommandManager getCommandManager() {
+        return this.commandManager;
     }
 
     public GatewayDiscordClient getClient() {
---
+++ src/main/java/io/github/boogiemonster1o1/legacyfabricbot/command/CommandManager.java
@@ -0,0 +1,79 @@
+package io.github.boogiemonster1o1.legacyfabricbot.command;
+
+import java.time.Instant;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
+import io.github.boogiemonster1o1.legacyfabricbot.LegacyFabricBot;
+import com.google.common.collect.ImmutableList;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.tree.CommandNode;
+
+public class CommandManager {
+    private final CommandDispatcher<MessageCreateEvent> dispatcher;
+
+    public CommandManager() {
+        this.dispatcher = new CommandDispatcher<>();
+        LegacyFabricBot.getInstance().getClient()
+                .on(MessageCreateEvent.class)
+                .filter(event -> event.getMember().isPresent() && !event.getMember().get().isBot())
+                .filter(event -> event.getMessage().getContent().charAt(0) == '$' && this.dispatcher.getRoot()
+                        .getChildren()
+                        .stream()
+                        .map(CommandNode::getName)
+                        .collect(ImmutableList.toImmutableList())
+                        .contains(event.getMessage()
+                                .getContent()
+                                .substring(1)
+                                .split(" ")[0]))
+                .subscribe(event -> {
+                    try {
+                        this.dispatcher.execute(event.getMessage().getContent().substring(1), event);
+                    } catch (CommandSyntaxException e) {
+                        error(event, e);
+                    }
+                });
+    }
+
+    public CommandDispatcher<MessageCreateEvent> getDispatcher() {
+        return this.dispatcher;
+    }
+
+    private void register(Factory factory) {
+        factory.register(this.getDispatcher());
+    }
+
+    public static LiteralArgumentBuilder<MessageCreateEvent> literal(String name) {
+        return LiteralArgumentBuilder.literal(name);
+    }
+
+    public static <T> RequiredArgumentBuilder<MessageCreateEvent, T> argument(String name, ArgumentType<T> argumentType) {
+        return RequiredArgumentBuilder.argument(name, argumentType);
+    }
+
+    public static void appendFooter(EmbedCreateSpec spec, MessageCreateEvent event) {
+        if (event.getMember().isPresent()) {
+            spec.setFooter("Requested by " + event.getMember().get().getUsername() + "#" + event.getMember().get().getDiscriminator(), event.getMember().get().getAvatarUrl());
+        }
+        spec.setTimestamp(Instant.now());
+    }
+
+    public static void error(MessageCreateEvent event, CommandSyntaxException e) {
+        event.getMessage().getChannel().block().createEmbed(spec -> {
+            spec.setTitle("Error parsing command: ")
+                    .setColor(Color.RED)
+                    .setDescription(e.getMessage());
+            appendFooter(spec, event);
+        }).block();
+    }
+
+    @FunctionalInterface
+    interface Factory {
+        void register(CommandDispatcher<MessageCreateEvent> dispatcher);
+    }
+}
