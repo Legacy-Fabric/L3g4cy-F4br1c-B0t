99e5ab024954d8db737997c828aa42f85aa17af6
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-09 10:01:01 +0530

    Working latest version finder
     Changes to be committed:
    	modified:   src/test/java/io/github/boogiemonster1o1/legacyfabricbot/URLTest.java

--- src/test/java/io/github/boogiemonster1o1/legacyfabricbot/URLTest.java
+++ src/test/java/io/github/boogiemonster1o1/legacyfabricbot/URLTest.java
@@ -2,6 +2,7 @@ package io.github.boogiemonster1o1.legacyfabricbot;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Iterator;
 
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
@@ -10,6 +11,7 @@ import org.dom4j.io.SAXReader;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class URLTest {
     @Test
@@ -17,6 +19,17 @@ public class URLTest {
         URL url = new URL("http://dl.bintray.com/legacy-fabric/Legacy-Fabric-Maven/net/fabricmc/yarn/maven-metadata.xml");
         Document doc = new SAXReader().read(url);
         Element root = doc.getRootElement();
-        assertEquals(root.getName() + " was not 'metadata'", "metadata", root.getName());
+        assertEquals("not 'metadata'", "metadata", root.getName());
+        String data = "";
+        Iterator<Element> iterator = root.element("versioning").element("versions").elementIterator();
+        while (iterator.hasNext()) {
+            Element next = iterator.next();
+            assertSame(next.getData().getClass(), String.class);
+            if (!next.getData().toString().startsWith("1.8.9")) {
+                continue;
+            }
+            data = next.getData().toString();
+        }
+        System.out.println(data);
     }
 }
