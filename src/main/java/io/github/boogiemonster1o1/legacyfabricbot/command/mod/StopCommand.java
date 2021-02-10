4b8d5363904aa0f690651589509c441e3d2dddc5
SD <harrypotter.starwars.shrish@gmail.com>
2021-02-10 10:38:34 +0530

    MEssage and thing

--- src/main/java/io/github/boogiemonster1o1/legacyfabricbot/command/mod/StopCommand.java
+++ src/main/java/io/github/boogiemonster1o1/legacyfabricbot/command/mod/StopCommand.java
@@ -1,9 +1,11 @@
 package io.github.boogiemonster1o1.legacyfabricbot.command.mod;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import io.github.boogiemonster1o1.legacyfabricbot.LegacyFabricBot;
 import io.github.boogiemonster1o1.legacyfabricbot.command.CommandManager;
 
 public class StopCommand {
@@ -16,7 +18,9 @@ public class StopCommand {
 
     private static int execute(CommandContext<MessageCreateEvent> ctx) throws CommandSyntaxException {
         CommandManager.checkPerm(ctx.getSource());
+        ctx.getSource().getMessage().getChannel().flatMap(channel -> channel.createMessage("Stopping!")).subscribe();
+        LegacyFabricBot.getInstance().getClient().logout();
         System.exit(0);
-        return 0;
+        return Command.SINGLE_SUCCESS;
     }
 }
